1f572ab379277e1cd5273443d78931115d36b4ba
MacbookPro <macbookpro@MacbookPros-MBP.home>
2020-04-29 15:15:20 -0500

    se envía primera version del key

---
+++ src/test/java/badcriado2/com/pe/monitoreo/AppTest.java
@@ -0,0 +1,27 @@
+package badcriado2.com.pe.monitoreo;
+
+import org.junit.*;
+
+public class AppTest {
+
+	@BeforeClass
+	public static void beforeClass() {
+	}
+
+	@AfterClass
+	public static void afterClass() {
+	}
+
+	@After
+	public void after() {
+	}
+
+	@Before
+	public void before() {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
